719635f6d02b7f8a31ec9c1a8d7a3ea438a59200
KozZzi-ru <AslamovAi@outlook.com>
2026-01-31 23:41:34 +0300

    Add trackball processor configuration files and update west.yml
    - Create charybdis_layers.h with layer definitions
    - Create charybdis_trackball_processors.dtsi with snipe/scroll/move modes
    - Update charybdis_right.overlay to use trackball processors
    - Update west.yml to use zmk-pmw3610-driver zmk-0.4 revision

---
+++ config/boards/shields/charybdis/charybdis_layers.h
@@ -0,0 +1,14 @@
+/*
+ * Layer definitions for Charybdis keyboard
+ * Must match keymap layer order
+ */
+
+#pragma once
+
+// Layer definitions matching your keymap
+#define QWERTY 0
+#define NUM_AND_FUN 1
+#define AUTO_MOUSE 2
+#define SNIPE 3
+#define CORRECTIONS 4
+#define SET_BT 5
